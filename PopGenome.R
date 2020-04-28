13863353844051f96419f8fe5cfeac6d97ac1ea3
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2020-04-28 18:54:16 -0500

    Update PopGenome.R

--- PopGenome.R
+++ PopGenome.R
@@ -3,20 +3,26 @@ rm(list = ls())
 library(tidyverse)
 library(PopGenome)
 
+###To calculate the total length of the loci, use the script size_loci.sh
+
 ##Load your vcf ##Your vcf must be in a separate folder
 data <- readData("vcf/", format = "VCF", include.unknown = TRUE, FAST = TRUE)
 
 ##load your popfile
 pops <- read.table("popfile.txt", head=T)
 ##add this value
-data<-set.populations(data,split(pops$ind, pops$pop),diploid=TRUE)
+##number of nSites
+n.snps<-data@n.biallelic.sites + data@n.polyallelic.sites
+##popassign
+populations<-split(as.character(pops$ind), pops$pop)
+data<-set.populations(data,populations,diploid=TRUE)
 ##Calculations
 # NEUTRALITY STATISTICS 
 data <- neutrality.stats(data, FAST=TRUE) 
 get.neutrality(data)[[1]] 
 data@Tajima.D 
 # FST 
-data <- F_ST.stats(data, mode="haplotype")
+data <- F_ST.stats(data)
 get.F_ST(data)[[1]] 
 data@nucleotide.F_ST 
 # DIVERSITY
@@ -28,6 +34,25 @@ data <-detail.stats(data,site.spectrum=TRUE,site.FST=TRUE)
 results <- get.detail(data) 
 data@region.stats@minor.allele.freqs
 
+nd<-data@nuc.diversity.within/size
+pops <- levels(pops$pop)
+colnames(nd) <- paste0(pops, "_pi")
+
+colnames(tajima) <- paste0(pops, "_tajima")
+
+w<-data@theta_Watterson/size
+colnames(w) <- paste0(pops, "_w")
+
+###Calculations by sites
+
+data <- diversity.stats(data, pi = TRUE)
+data_nuc_div <- t(data@region.stats@nuc.diversity.within[[1]])
+nd_site<-nd_site<-as.data.frame(t(apply(data_nuc_div,2,mean)))
+colnames(nd_site) <- paste0(pops, "_pi")
+
+
+
+
 # Concatenate loci
 CON <- concatenate.regions(data) 
 CON <- detail.stats(CON,site.spectrum=TRUE,site.FST=TRUE) 
@@ -36,6 +61,6 @@ allele_Freqs <- CON@region.stats@minor.allele.freqs[[1]]
 freq.table <- list()
 freq.table[[1]] <- table(allele_Freqs) 
 sfs <- data.frame(freq.table)
-
+sfs<-sfs[-1,]##Remove missing data
 library(ggplot2) 
 ggplot(sfs, aes(x=allele_Freqs, y=Freq)) + geom_bar(stat = 'identity')
