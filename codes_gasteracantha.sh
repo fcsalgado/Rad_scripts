4b929d7020583732ba93794fb8a52dabe499adf7
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2020-10-20 15:51:34 -0500

    Update codes_gasteracantha.sh

--- codes_gasteracantha.sh
+++ codes_gasteracantha.sh
@@ -1,4 +1,5 @@
-for i in $(cat new_key.txt);
-do s=$(grep -E "$i" new_key.txt | cut -f 2)
-n=$(grep -E "^$s\_" codes.txt)
-sed -i -E "s/(\s)$i$/\1$n/g" new_key2.txt; done
+for i in $(cat spider_key.txt);
+do s=$(grep -E "$i" spider_key.txt| awk 'NR==1' | cut -f 2)
+w=$(grep -E "$i" spider_key.txt | awk 'NR==1'| cut -f 1)
+n=$(grep -E "\_$s$" spider_key2.txt)
+sed -i "s/$w/$n/g" cabeza.txt ; done
