e6f52a85daa152136e40c072fd8e577753b1fd8c
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2021-01-07 09:25:04 -0500

    Update vcftools_filtering.sh

--- vcftools_filtering.sh
+++ vcftools_filtering.sh
@@ -4,6 +4,12 @@ for i in $(cat new_key.txt);
 do s=$(grep $i new_key.txt | cut -f 1)
 n=$(grep $i new_key.txt | cut -f 2)
 sed -i -r "s/\w+\_$s\_\w+_\w+/$n/g" 090_filtered.vcf; done #look that I'm using some regular expressions here, this is not necessary in all the cases
+######
+for i in $(cut -f 1 out.imiss);        
+do s=$(echo "$i" | sed -E 's/(\w+)_sorted/\1/g')
+n=$(grep -E "_$s$" spider_key2.txt)
+sed -iE "s/\t$i\t/\t$n\t/g" head.txt ; done
+#######
 
 ### First you have to filter your vcf for variants that have been succsfully genoetyped in 70% of individuals (--max-missing 0.70) and fileter SNPs that have a minor allele count less than 3 (--mac 3), and a limit leave just quality SNPs --minQ 30
 module load vcftools/0.1.16
