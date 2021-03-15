602156032c27160cedbf0920f214a025604f8a0a
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2021-03-15 14:26:56 -0500

    Update treemix.sh

--- treemix.sh
+++ treemix.sh
@@ -18,8 +18,9 @@ python2 /home/fabian/software/treemix-1.13/src/plink2treemix.py plink.frq.strat.
 #bootstrapt over this topology (optional)
 /home/fabian/software/treemix-1.13/src/./treemix -i treemix_input.frq.gz -root mutata -bootstrap -k 100 -o out_topology
 ##you have to cover a range of possible migration events, lets test fo 1 to 6
-for m in $(echo {1..6});  
-do /home/fabian/software/treemix-1.13/src/./treemix -i treemix_input.frq.gz -m $m -g out_topology.vertices.gz out_topology.edges.gz -o out_migration"$m";done
+for i in in $(echo {1..10}); do
+for m  in $(echo {1..6});  
+do /home/fabian/software/treemix-1.13/src/./treemix -i treemix_input.frq.gz -m $m -g out_topology.vertices.gz out_topology.edges.gz -o out_migration"$m"_"$i";done;done
 
 ##Explore the llik files to look for the best value of m, the open R and plot the result
 source("/home/fabian/software/treemix-1.13/src/plotting_funcs.R")
