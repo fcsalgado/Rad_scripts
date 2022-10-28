14e9e5b9ef4bd8f635e63e05606b5c25ce075ebc
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2022-10-28 16:23:36 +1100

    Update best_runs.sh

--- best_runs.sh
+++ best_runs.sh
@@ -3,6 +3,10 @@
 # Bash script by Joana Meier to select the best fastsimcoal run of multiple runs under the same demographic model
 # The script expects output files of different runs to be found in folders starting with run
 
+mkdir best_like
+for sf in {1..100}; do
+cd sfs"$sf"
+#rm gasteracantha_five_93*
 m=-1000000000000000
 p=$1
 c=0
@@ -10,12 +14,12 @@ best="xxxx"
 diff=0
 diffBest=0
 
-for i in run*;
+for i in $(ls *.est | sed "s/.est//g");
 do
- a=$(ls | grep ${p}".tpl" | sed s'/.tpl//')
+ a=$(ls | grep $i".tpl" | sed s'/.tpl//')
 
  # if the file is in the run directory
- if [ -e $i/$a.bestlhoods ]
+ if [ -e "$i"/$a.bestlhoods ]
  then
    l=$(cat $i/$a.bestlhoods | awk '{print $(NF-1)}' | tail -1 | cut -f 1 -d ".")
    diff=$(cat $i/$a.bestlhoods | awk '{print $NF-$(NF-1)}' | tail -1 | cut -f 1 -d ".")
@@ -28,7 +32,7 @@ do
      diff=$(cat $i/$a/$a.bestlhoods | awk '{print $NF-$(NF-1)}' | tail -1 | cut -f 1 -d ".")
      ((c++))
    else
-     echo "no .bestlhoods file found in "$i
+     echo "no .bestlhoods file found in "$i 
    fi
 
  fi
@@ -43,9 +47,9 @@ done
 
 if [ -z ${x+1} ]
 then echo "Error: No run with lik>-1000000000000000"
-else mkdir bestrun
+else mkdir -p bestrun
 cp $x/* ./bestrun/
-cp $x/$a/* ./bestrun/
+cp ./bestrun/*.bestlhoods ../best_like/sfs"$sf".bestlhoods
 fi
-
 echo -e "\n"$c" bestlhoods files found, "$best" with "$diffBest" Lhood diff fits best."
+cd ../;done
