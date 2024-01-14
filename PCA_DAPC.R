eb038e59b3d7b7f2da71890602e2891163cbe807
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2024-01-15 10:23:46 +1100

    Update PCA_DAPC.R

--- PCA_DAPC.R
+++ PCA_DAPC.R
@@ -1,55 +1,44 @@
+# Load necessary libraries
 library(vcfR)
 library(adegenet)
 library(phrynomics)
 
-vcf<-read.vcfR("random_vcf.vcf",verbose=F)
-my_genind <- vcfR2genind(vcf)
-pop<-read.table("pop.txt",head=F)
-clases<-c("Campinas","PFBA","Lencois","Acre","Leticia","Guaviare","Tarapoto","Moyobamba","Sucua","Misahualli","Jaen","Villavicencio","ElPangui","Gualaquiza","Banos","Alamor","Vilcabamba","Piura","Chiclayo","Lima","Santo","Quito","Bahia","Cali","Palmira","Boquia","Ibague","Medellin","Cucuta","Cartagena","Sanjuan","San")
-pop$V1<-factor(pop$V1, levels=clases)
-#d<-t(as.data.frame(strsplit(nom, "\\_")))
-#pop<-as.vector(d[,2])
-my_genind$pop<-as.factor(pop$V1)
-##Calculate tyhe number of clusters
-grp <- find.clusters(my_genind, max.n.clust=40)
-###Show your new groups based on your original pops
-table.value(table(pop(my_genind), grp$grp), col.lab=paste("inf", 1:7))
+# Read the VCF file containing random SNPs
+vcf <- read.vcfR("<your_vcf>", verbose = FALSE)
 
-##DAPC
+# Convert VCF to genind format
+my_genind <- vcfR2genind(vcf)
+
+# Read population information from a text file
+pop <- read.table("pop.txt", head = FALSE)
+
+# Define population classes and set levels
+clases <- c("Campinas", "PFBA", "Lencois", "Acre", "Leticia", "Guaviare", "Tarapoto", "Moyobamba", "Sucua", "Misahualli", "Jaen", "Villavicencio", "ElPangui", "Gualaquiza", "Banos", "Alamor", "Vilcabamba", "Piura", "Chiclayo", "Lima", "Santo", "Quito", "Bahia", "Cali", "Palmira", "Boquia", "Ibague", "Medellin", "Cucuta", "Cartagena", "Sanjuan", "San")
+pop$V1 <- factor(pop$V1, levels = clases)
+
+# Assign population information to genind object
+my_genind$pop <- as.factor(pop$V1)
+
+# Calculate the number of clusters using find.clusters
+grp <- find.clusters(my_genind, max.n.clust = 40)
+
+# Display the distribution of original populations within the clusters
+table.value(table(pop(my_genind), grp$grp), col.lab = paste("inf", 1:7))
+
+# DAPC analysis
 dapc.my_genind <- dapc(my_genind, var.contrib = TRUE, scale = FALSE, n.pca = 30, n.da = nPop(my_genind) - 1)
 
+# Save DAPC results to a PDF file
 pdf("DAPC.pdf", width = 20, height = 27, family = "Helvetica")
 
-scatter(dapc.my_genind, col=c("#68af57","#fb6ea0","#00cec3"))
+# Plot DAPC results with specified colors
+scatter(dapc.my_genind, col = c("#68af57", "#fb6ea0", "#00cec3"))
 
-##crossvalidation
-
+# Cross-validation using xvalDapc
 set.seed(999)
-my_genind$pop<-grp$grp
-pramx <- xvalDapc(tab(my_genind, NA.method = "mean"), pop(my_genind),training.set=0.9,result="groupMean",center=T,scale=F,n.pca=NULL,n.rep=100)
+my_genind$pop <- grp$grp
+pramx <- xvalDapc(tab(my_genind, NA.method = "mean"), pop(my_genind), training.set = 0.9, result = "groupMean", center = TRUE, scale = FALSE, n.pca = NULL, n.rep = 100)
 
-scatter(pramx$DAPC,cex = 2, legend = TRUE,
-clabel = FALSE, posi.leg = "bottomleft", col=c("#00cec3","#68af57","#fb6ea0"),
-posi.pca = "topleft", cleg = 0.75, xax = 1, yax = 2, inset.solid = 1)
+# Plot cross-validation results
+scatter(pramx$DAPC, cex = 2, legend = TRUE, clabel = FALSE, posi.leg = "bottomleft", col = c("#00cec3", "#68af57", "#fb6ea0"), posi.pca = "topleft", cleg = 0.75, xax = 1, yax = 2, inset.solid = 1)
 
-###pca
-
-X <- scaleGen(my_genind, NA.method="mean")
-
-pca1 <- dudi.pca(X,cent=FALSE,scale=FALSE,scannf=FALSE,nf=3)
-
-barplot(pca1$eig[1:50],main="PCA eigenvalues", col=heat.colors(50))
-
-col <- funky(15)
-
-##Plot PCA
-
-s.class(pca1$li, pop(my_genind),xax=1,yax=2, col=c("#68af57","#fb6ea0","#00cec3"), axesell=FALSE,cstar=0, cpoint=3, grid=FALSE)
-
-#1east="#68af57"
-#2west="#fb6ea0"
-#3dry="#00cec3"
-
-pdf("DAPC_filtered.pdf", width = 20, height = 27, family = "Helvetica")
-
-scatter(dapc.my_genind, col=c("#68af57","#fb6ea0","#00cec3"))
