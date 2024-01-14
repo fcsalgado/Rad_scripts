1b85e7ed93bfd3fe0c6fd3cc6aef14e914ddc60e
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2024-01-15 10:15:49 +1100

    Update random_snps_rad.sh

--- random_snps_rad.sh
+++ random_snps_rad.sh
@@ -1,19 +1,26 @@
-#! /bin/bash
+#!/bin/bash
 
-#To run this script you hace to execute it follow by the name of your VCF file
+# Usage: ./random_snp.sh <your_vcf_file>
 
-file=${1?Error: no given name}
-#create a file with the number of SNPs per locus, replace the awk number by the number of header lines
-grep "#" $file > random_vcf.vcf
+# Check if a file name is provided
+file=${1?Error: no file name provided}
+
+# Create a file with the number of SNPs per locus (excluding header lines)
+grep "#" "$file" > random_vcf.vcf
 lines=$(wc -l random_vcf.vcf | cut -f 1 -d " ")
-#calculate the repetitions per locus
-cut -f 1 $file | awk -v var=$lines 'NR > var' | sort | uniq -c | sed -E 's/^\s+(\w+)/\1/g' > counts_by_loci.txt
 
-#select randon one nucleotide
+# Calculate the repetitions per locus
+cut -f 1 "$file" | awk -v var="$lines" 'NR > var' | sort | uniq -c | sed -E 's/^\s+(\w+)/\1/g' > counts_by_loci.txt
+
+# Select a random nucleotide for each locus
 cat counts_by_loci.txt | while read i;
 do
-number=$(grep "$i" counts_by_loci.txt | cut -d " " -f 1)
-locus=$(grep "$i" counts_by_loci.txt | cut -d " " -f 2)
-grep "$locus" $file | shuf -n 1 >> random_vcf.vcf; done
+  number=$(grep "$i" counts_by_loci.txt | cut -d " " -f 1)
+  locus=$(grep "$i" counts_by_loci.txt | cut -d " " -f 2)
+  grep "$locus" "$file" | shuf -n 1 >> random_vcf.vcf
+done
 
+# Clean up temporary files
 rm counts_by_loci.txt
+
+echo "Random SNP selection completed. Output saved to random_vcf.vcf."
