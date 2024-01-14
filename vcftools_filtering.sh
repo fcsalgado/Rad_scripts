d7a33a4fc2cc05da8290f77667703d72ad88cd53
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2024-01-15 10:10:07 +1100

    Update vcftools_filtering.sh

--- vcftools_filtering.sh
+++ vcftools_filtering.sh
@@ -11,42 +11,34 @@ n=$(grep -E "_$s$" spider_key2.txt)
 sed -iE "s/\t$i\t/\t$n\t/g" head.txt ; done
 #######
 
-### First you have to filter your vcf for variants that have been succsfully genoetyped in 70% of individuals (--max-missing 0.70) and fileter SNPs that have a minor allele count less than 3 (--mac 3), and a limit leave just quality SNPs --minQ 30
-module load vcftools/0.1.16
+### Step 1: Filter VCF for variants with successful genotyping, a minor allele count ≥ 3, and a minimum quality of 30.
 
+module load vcftools/0.1.16
 vcftools --vcf <your_vcf> --max-missing 0.70 --mac 3 --minQ 30 --recode --recode-INFO-all --out <your_filtered_vcf1>
 
-##Remove indels or variants that are not bi-allelic --min-alleles 2 --max-alleles 2 and --remove-indels
+### Step 2: Remove indels and non-bi-allelic variants.
 
 vcftools --vcf <your_filtered_vcf1> --recode --min-alleles 2 --max-alleles 2 --remove-indels --recode-INFO-all --out <your_filtered_vcf2>
 
-#If you want to filter individuals with high missing data
+### Optional: Filter individuals with high missing data (>50%).
 
-#Check the amount of missing data across your samples, look the out.imiss file genetated
-
+# Check missing data across samples.
 vcftools --vcf <your_filtered_vcf2> --missing-indv
 
-#Remove individuals with missing data higher that 50%
-
+# Remove individuals with missing data >50%.
 awk '$5 > 0.5' out.imiss | cut -f1 > lowDP.indv
-
 vcftools --vcf <your_filtered_vcf2> --remove lowDP.indv --recode --recode-INFO-all --out <your_filtered_vcf3>
 
-###If you want to keep high quality snps, restrict the data to variants called in a high percentage of individuals (say 90%), minor allele frequency (0.05) and filter by mean depth of genotypes (20)
+### Step 3: Further filter for high-quality SNPs (90% call rate, 0.05 MAF, and mean depth of genotypes ≥ 20).
 
-vcftools --vcf <your_filtered_vcf3> --max-missing 0.95 --maf 0.05 --min-meanDP 20 --recode --recode-INFO-all  --out <your_filtered_vcf4>
+vcftools --vcf <your_filtered_vcf3> --max-missing 0.95 --maf 0.05 --min-meanDP 20 --recode --recode-INFO-all --out <your_filtered_vcf4>
 
-#Create a new VCF for your admixture or structure analysis 
-
-##Filter by hwe
+### Step 4: Filter by Hardy-Weinberg Equilibrium (HWE) and create a new VCF.
 
 vcftools --vcf <your_filtered_vcf3> --hwe 0.05 --recode --out <vcf_hwe_snps>
 
-##Remember to select one snps per tag, for that you have to run the random_snp.sh script ;)
-##Convert your VCF to plink
+### Step 5: Select one SNP per tag (use random_snp.sh script) and convert VCF to Plink format.
 
 vcftools --vcf <vcf_hwe_snps> --plink --out <plink_hwe>
-
-##
-
 plink --file <plink_hwe> --make-bed --out <plink_for_admixture>
+
