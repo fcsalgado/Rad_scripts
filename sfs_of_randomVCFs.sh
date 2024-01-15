4f0f2350f630f639dbf5b267b1ab02525c5f7512
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2024-01-15 14:45:13 +1100

    Update sfs_of_randomVCFs.sh

--- sfs_of_randomVCFs.sh
+++ sfs_of_randomVCFs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+# Specify the number of parallel processes
 N=10
-#select randon one nucleotide
-for k in $(echo {1..100}); do
-((i=i%N)); ((i++==0)) && wait
-python3 easySFS.py -i random_$k.vcf -p fivepops_sorted.txt --ploidy 2 --proj 20,34,15,10,35 -f -o random_$k; done
+
+# Step 1: Iterate through 100 random VCF files
+for k in $(seq 1 100); do
+    # Avoid race condition by limiting the number of parallel processes
+    ((i=i%N)); ((i++==0)) && wait
+
+    # Step 2: Run easySFS.py on each random VCF file
+    python3 easySFS.py -i random_$k.vcf -p fivepops_sorted.txt --ploidy 2 --proj 20,34,15,10,35 -f -o random_$k
+done
