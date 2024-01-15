8979046b8ee12e84d4cbb73806f2d1bb821b66b7
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2024-01-15 11:40:58 +1100

    Update run_raxml.sh

--- run_raxml.sh
+++ run_raxml.sh
@@ -1,36 +1,19 @@
-### First you have to filter your vcf for
-module load vcftools/0.1.16
+# Script for Transforming VCF to Phylip Format, Correcting Branch Lengths, and Running RAxML
 
-vcftools --vcf <your_vcf> --max-missing 0.55 --mac 3 --recode --recode-INFO-all --out <your_filtered_vcf>
+## Note: Ensure that the required software (python, RAxML) is installed and available in your PATH.
 
-#then trnsform that vcf to phylip using the vcf2phylip.py script obtained from: https://github.com/edgardomortiz/vcf2phylip/blob/master/vcf2phylip.py
-
+# Transform VCF to Phylip using vcf2phylip.py script
+# Note: Obtain the vcf2phylip.py script from https://github.com/edgardomortiz/vcf2phylip/blob/master/vcf2phylip.py
 python ~/shared/polythore_total/old_plate/scripts/vcf2phylip.py --input <your_filtered_vcf>
 
-###With obteined phylyip your have to implemet a correction to the branch length, prepare the input using this script
+# Implement a correction to the branch length using ascbias.py script
+# Note: Obtain the ascbias.py script from https://github.com/btmartin721/raxml_ascbias/tree/d13d6eee640b732c0529c9c94cd99cd8838d9b7a
+# Note: Ascertain the correct path to the ascbias.py script
 module load py34-biopython/1.71
-
 module load python3.6/3.6.6
-
 module swap py3-numpy py36-numpy/1.14.2
 
 python3 ~/shared/polythore_total/old_plate/scripts/ascbias.py -p polythore_filtered.min4.recode.min4.phy
 
-##run RAxML
-
+# Run RAxML for phylogenetic analysis
 raxmlHPC-PTHREADS -T 30 -f a -# 100 -m ASC_GTRGAMMA --asc-corr=lewis -p 12345 -x 12345 -s variable_sites.phy -n polythore_tree.tre
-
-####change the tips names using the key files given by snpsaurus
-
-for i in $(cut -f 1 new_key.txt);do
-x=$(grep $i new_key.txt | cut -f 2)
-sed -i "s/$i/$x/g" RAxML_bipartitions.polythore_tree.tre;
-done
-
-####useful filtering command in vcftools
-
-vcftools --vcf <your.vcf> --missing-indv
-
-mawk '$5 > 0.5' <your.vcf> | cut -f1 > lowDP.indv
-
-vcftools --vcf <your.vcf> --remove lowDP.indv --recode --recode-INFO-all --out remove_filtered
