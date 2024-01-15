1c3ebbfa3b5b5ca293afc167208a0486a3a95d60
Fabian_Salgado_Roa <fcsalgador@gmail.com>
2024-01-15 14:48:34 +1100

    Update add_invSites_SFS.sh

--- add_invSites_SFS.sh
+++ add_invSites_SFS.sh
@@ -1,10 +1,28 @@
-#run this script within vcfs folder
-#Let's create the header for all the SFS
+#!/bin/bash
+
+# Run this script within the vcfs folder
+
+# Step 1: Create the header for all the SFS
 awk 'NR<3' random_"$k"/fastsimcoal2/random_"$k"_MSFS.obs > ../all_SFS/header.txt
-for k in $(echo {1..100}); do 
-	((i=i%N)); ((i++==0)) && wait
-	first=$(awk -F " " 'NR==3 {print $1}' random_"$k"/fastsimcoal2/random_"$k"_MSFS.obs)
-	inv_sites=$(python -c "print($first+75000)") ##Add the invaribale sites
-	paste <(echo $inv_sites) <(awk 'NR==3' random_"$k"/fastsimcoal2/random_"$k"_MSFS.obs | cut -d " " -f 2-) --delimiters ' ' >> ../all_SFS/body.txt
-	cat ../all_SFS/header.txt ../all_SFS/body.txt > ../all_SFS/random_"$k"_MSFS.obs
-	rm ../all_SFS/body.txt; done
+
+# Specify the number of parallel processes
+N=10
+
+# Step 2: Iterate through 100 random simulations
+for k in $(seq 1 100); do 
+    # Avoid race condition by limiting the number of parallel processes
+    ((i=i%N)); ((i++==0)) && wait
+
+    # Step 3: Extract information and add invariable sites
+    first=$(awk -F " " 'NR==3 {print $1}' random_"$k"/fastsimcoal2/random_"$k"_MSFS.obs)
+    inv_sites=$(python -c "print($first+75000)") ## Add the invariable sites, calculate this carefully considering the number of snps and size of the tags used
+
+    # Step 4: Paste the information to the body file
+    paste <(echo $inv_sites) <(awk 'NR==3' random_"$k"/fastsimcoal2/random_"$k"_MSFS.obs | cut -d " " -f 2-) --delimiters ' ' >> ../all_SFS/body.txt
+
+    # Step 5: Combine header and body to create the final MSFS.obs file
+    cat ../all_SFS/header.txt ../all_SFS/body.txt > ../all_SFS/random_"$k"_MSFS.obs
+
+    # Step 6: Clean up temporary body file
+    rm ../all_SFS/body.txt
+done
